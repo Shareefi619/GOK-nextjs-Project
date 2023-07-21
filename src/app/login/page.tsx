5f4f4d623f4dee9a7cf602d9e18b8598eea357ed
Shareefi619 <irfanhaidershareefi6@gmail.com>
2023-07-21 18:17:01 +0500

    6th commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,77 +1,178 @@
 "use client";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import { toast } from "react-hot-toast";
-import { debug } from "console";
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
 
-export default function SignupPage() {
-  const router = useRouter();
-  const [user, setUser] = useState({
-    email: "",
-    password: "",
+interface ILoginForm {
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+export default function LoginPage() {
+  const { handleSubmit, control, getValues } = useForm<ILoginForm>({
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
   });
-  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const router = useRouter();
+
   const [loading, setLoading] = useState(false);
-  const onLogin = async () => {
+
+  const onLogin: SubmitHandler<ILoginForm> = async (data) => {
     try {
       setLoading(true);
-      const res = await axios.post("/api/users/login", user);
-      console.log(res);
-      // console.log(res.message);
+      const res = await axios.post("/api/users/login", data);
       if (res.status === 400) {
         throw new Error("Login failed");
       } else {
-        console.log("Login Success", res.data);
         toast.success("Login Success");
         router.push("/profile");
       }
     } catch (err: any) {
-      alert(err.message);
       toast.error(err.message);
     } finally {
       setLoading(false);
     }
   };
 
-  useEffect(() => {
-    if (user.email.length > 0 && user.password.length > 0) {
-      setButtonDisabled(false);
-    } else {
-      setButtonDisabled(true);
-    }
-  }, [user]);
-
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h2>{loading ? "Processing" : "Login"}</h2>
-      <hr />
-      <label htmlFor="email">email</label>
-      <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-300"
-        type="text"
-        id="email"
-        value={user.email}
-        onChange={(e) => setUser({ ...user, email: e.target.value })}
-        placeholder="email"
-      />
-      <label htmlFor="password">password</label>
-      <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-300"
-        type="password"
-        id="password"
-        value={user.password}
-        onChange={(e) => setUser({ ...user, password: e.target.value })}
-        placeholder="password"
-      />
-      <button
-        onClick={onLogin}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Login
-      </button>
-      <Link href="/signup">Visit Signup Page</Link>
-    </div>
+    <form onSubmit={handleSubmit(onLogin)}>
+      {/* <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h2>{loading ? "Processing" : "Login"}</h2>
+        <hr />
+        <label htmlFor="email">email</label>
+        <Controller
+          control={control}
+          name="email"
+          rules={{
+            required: { value: true, message: "Email is required!" },
+            pattern: {
+              value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+              message: "Invalid Email",
+            },
+          }}
+          render={({ field: { onChange, value }, fieldState: { error } }) => (
+            <>
+              <input
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-300"
+                type="text"
+                value={value}
+                onChange={onChange}
+                placeholder="email"
+              />
+              <span>{error?.message}</span>
+            </>
+          )}
+        />
+        <label htmlFor="password">password</label>
+
+        <Controller
+          control={control}
+          name="password"
+          rules={{
+            required: { value: true, message: "Password is required!" },
+          }}
+          render={({ field: { onChange, value }, fieldState: { error } }) => (
+            <>
+              <input
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-300"
+                type="text"
+                value={value}
+                onChange={onChange}
+                placeholder="password"
+              />
+              <span>{error?.message}</span>
+            </>
+          )}
+        />
+        
+        <button
+          type="submit"
+          disabled={loading}
+          className={`px-4 py-2 rounded-md text-black bg-blue-500 hover:bg-blue-60 disabled:opacity-50 disabled:cursor-not-allowed`}
+        >
+          Login
+        </button>
+        <Link href="/signup">Visit Signup Page</Link>
+      </div> */}
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <div className="w-full max-w-md bg-neutral-700 p-8 rounded-lg shadow-md">
+          <h2 className="text-2xl font-semibold mb-4 items-center text-center">
+            {loading ? "Processing" : "Login"}
+          </h2>
+          <hr className="mb-4" />
+          <label htmlFor="email" className="flex mb-5 font-medium">
+            Email
+          </label>
+          <Controller
+            // ... rest of the Controller code for email input
+            control={control}
+            name="email"
+            rules={{
+              required: { value: true, message: "Email is required!" },
+              pattern: {
+                value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                message: "Invalid Email",
+              },
+            }}
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
+              <>
+                <input
+                  className="flex p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-300"
+                  type="text"
+                  value={value}
+                  onChange={onChange}
+                  placeholder="email"
+                />
+                <span>{error?.message}</span>
+              </>
+            )}
+          />
+          <label htmlFor="password" className="flex mt-4 mb-2 font-medium">
+            Password
+          </label>
+          <Controller
+            // ... rest of the Controller code for password input
+            control={control}
+            name="password"
+            rules={{
+              required: { value: true, message: "Password is required!" },
+            }}
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
+              <>
+                <input
+                  className="flex p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-300"
+                  type="text"
+                  value={value}
+                  onChange={onChange}
+                  placeholder="password"
+                />
+                <span>{error?.message}</span>
+              </>
+            )}
+          />
+          <button
+            type="submit"
+            disabled={loading}
+            className={`px-4 py-2 rounded-md text-white bg-blue-500 hover:bg-blue-600 ${
+              loading ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+          >
+            {loading ? "Processing" : "Login"}
+          </button>
+          <p className="mt-4">
+            <Link href="/signup" className="text-blue-500 hover:underline">
+              Visit Signup Page
+            </Link>
+          </p>
+        </div>
+      </div>
+    </form>
   );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,81 +4,194 @@ import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import { toast } from "react-hot-toast";
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
+interface ISignupForm {
+  username: string;
+  email: string;
+  password: string;
+}
 
 export default function SignupPage() {
-  const router = useRouter();
-  const [buttonDisabled, setButtonDisabled] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [user, setUser] = useState({
-    email: "",
-    password: "",
-    username: "",
+  const { handleSubmit, control, getValues } = useForm<ISignupForm>({
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
   });
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    if (
-      user.email.length > 0 &&
-      user.password.length > 0 &&
-      user.username.length > 0
-    ) {
-      setButtonDisabled(false);
-    } else {
-      setButtonDisabled(true);
-    }
-  }, [user]);
+  // useEffect(() => {
+  //   if (
+  //     user.email.length > 0 &&
+  //     user.password.length > 0 &&
+  //     user.username.length > 0
+  //   ) {
+  //     setButtonDisabled(false);
+  //   } else {
+  //     setButtonDisabled(true);
+  //   }
+  // }, [user]);
 
-  const onSignup = async () => {
+  const onSignup: SubmitHandler<ISignupForm> = async (data) => {
     try {
       setLoading(true);
-      console.log(user)
-      const res = await axios.post("/api/users/signup", user);
-      console.log("Signup Success", res.data);
-      router.push("/login")
-    } catch (error: any) {
-      console.log("signup failed", error);
-      toast.error(error.message);       
+      const res = await axios.post("/api/users/signup", data);
+      if (res.status === 400) {
+        throw new Error("Signup failed");
+      } else {
+        toast.success("Signup Success");
+        router.push("/login");
+      }
+    } catch (err: any) {
+      toast.error(err.message);
     } finally {
       setLoading(false);
     }
   };
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h2>{loading ? "Processing" : "Signup"}</h2>
-      <hr />
-      <label htmlFor="username">username</label>
-      <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-300"
-        type="text"
-        id="username"
-        value={user.username}
-        onChange={(e) => setUser({ ...user, username: e.target.value })}
-        placeholder="username"
-      />
-      <label htmlFor="email">email</label>
-      <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-300"
-        type="text"
-        id="email"
-        value={user.email}
-        onChange={(e) => setUser({ ...user, email: e.target.value })}
-        placeholder="email"
-      />
-      <label htmlFor="password">password</label>
-      <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-300"
-        type="password"
-        id="password"
-        value={user.password}
-        onChange={(e) => setUser({ ...user, password: e.target.value })}
-        placeholder="password"
-      />
-      <button
-        onClick={onSignup}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        {buttonDisabled ? "No signup" : "Signup"}
-      </button>
-      <Link href="/login">Visit Login Page</Link>
-    </div>
+    <form onSubmit={handleSubmit(onSignup)}>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <div className="w-full max-w-md bg-neutral-700 p-8 rounded-lg shadow-md">
+          <h2 className="text-2xl font-semibold mb-4 text-center">
+            {loading ? "Processing" : "Signup"}
+          </h2>
+          <hr className="mb-4" />
+            <label htmlFor="username" className="flex mb-2 font-medium">
+              Username
+            </label>
+            <Controller
+              // ... rest of the Controller code for username input
+              control={control}
+              name="username"
+              rules={{
+                required: { value: true, message: "Username is required!" },
+                pattern: {
+                  value: /^[A-Za-z][A-Za-z0-9_]{7,29}$/,
+                  message: "Invalid Username",
+                },
+              }}
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <>
+                  <input
+                    className="flex p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-300"
+                    type="text"
+                    value={value}
+                    onChange={onChange}
+                    placeholder="username"
+                  />
+                  <span>{error?.message}</span>
+                </>
+              )}
+            />
+            <label htmlFor="email" className="flex mt-2 mb-2 font-medium">
+              Email
+            </label>
+            <Controller
+              // ... rest of the Controller code for email input
+              control={control}
+              name="email"
+              rules={{
+                required: { value: true, message: "Email is required!" },
+                pattern: {
+                  value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                  message: "Invalid Email",
+                },
+              }}
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <>
+                  <input
+                    className="flex p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-300"
+                    type="text"
+                    value={value}
+                    onChange={onChange}
+                    placeholder="email"
+                  />
+                  <span>{error?.message}</span>
+                </>
+              )}
+            />
+            <label htmlFor="password" className="flex mt-1 mb-2 font-medium">
+              Password
+            </label>
+            <Controller
+              // ... rest of the Controller code for password input
+              control={control}
+              name="password"
+              rules={{
+                required: { value: true, message: "Password is required!" },
+              }}
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <>
+                  <input
+                    className="flex p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-300"
+                    type="text"
+                    value={value}
+                    onChange={onChange}
+                    placeholder="password"
+                  />
+                  <span>{error?.message}</span>
+                </>
+              )}
+            />
+            <label htmlFor="confirmPassword" className="flex mt-4 mb-2 font-medium">
+              Confirm Password
+            </label>
+            <Controller
+              // ... rest of the Controller code for confirmPassword input
+              control={control}
+              name="changePassword"
+              rules={{
+                required: { value: true, message: "Password is required!" },
+                validate: {
+                  checkPassword: (value) =>
+                    value === getValues("password") || "Password not match",
+                },
+              }}
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <>
+                  <input
+                    className="flex p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-300"
+                    type="text"
+                    value={value}
+                    onChange={onChange}
+                    placeholder="password"
+                  />
+                  <span>{error?.message}</span>
+                </>
+              )}
+            />
+          <button
+            type="submit"
+            disabled={loading}
+            className={`flex px-4 py-2 rounded-md text-white bg-blue-500 hover:bg-blue-600 ${
+              loading ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+          >
+            {loading ? "No signup" : "Signup"}
+          </button>
+
+          <p className="mt-4">
+            <Link href="/login" className="text-blue-500 hover:underline">
+              Visit Login Page
+            </Link>
+          </p>
+        </div>
+      </div>
+    </form>
   );
 }
